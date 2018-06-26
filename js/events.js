6759c898228101daedb1525843f8bb76cce5c3b4
Samantha Sconyers <samanthasconyers@gmail.com>
2018-06-26 22:40:02 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -2,30 +2,26 @@
 
 function getIt() {
   $('p').on('click', function() {
-    alert('hey!')
+    alert('hey!');
   })
 }
-
 function frameIt() {
   $('img').on('load', function() {
-    $('img').addClass('tasty')
+    $('img').addClass('tasty');
   })
 }
-
 function pressIt() {
   $('#typing').on('keydown', function(e) {
     if (e.which === 71) {
-      alert("G")
+      alert("G");
     }
   })
 }
-
 function submitIt() {
   $('form').on('submit', function(e) {
-    alert('Your form is going to be submitted now.')
+    alert('Your form is going to be submitted now.');
   })
 }
-
 $(document).ready(function(){
 
 // call functions here
