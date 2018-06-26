4853124ed2ce7ebf887075782fb66b127d631b74
Samantha Sconyers <samanthasconyers@gmail.com>
2018-06-26 22:34:12 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -12,10 +12,9 @@ $(document).ready(function(){
   }
   function pressIt() {
     $('#typing').on('keydown', function(e) {
-      if e.which == 71 {
+      if (e.which == 71) {
       alert ("G")
-        
-      }
+      }}
   }
   
 });
