6727ce778e97771f127639c4c9e673b2db1eb165
Samantha Sconyers <samanthasconyers@gmail.com>
2018-06-26 22:39:23 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -14,9 +14,8 @@ function frameIt() {
 
 function pressIt() {
   $('#typing').on('keydown', function(e) {
-    // 'g' corresponds to 71
     if (e.which === 71) {
-      alert("You pressed 'g'!")
+      alert("G")
     }
   })
 }
